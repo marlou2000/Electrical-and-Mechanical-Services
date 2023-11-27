d69a3d6c0c24014165e4d101586b2a11a51adb96
Jeffrey8132000 <149402793+Jeffrey8132000@users.noreply.github.com>
2023-11-27 21:47:11 +0800

    Created Navbar & Marlou circle
    Created Navbar & Marlou circle

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,11 +9,17 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
 
         @vite('resources/css/app.css')
 
     </head>
     <body class="antialiased">
-
+        {{$slot}}
+        @livewireScripts
+        <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
     </body>
 </html>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,143 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="homebody">
+
+<!-- Main navigation container -->
+<nav
+id="navbar"
+  class="z-50 fixed mx-0 flex flex-wrap items-center justify-between text-neutral-500 transition-colors duration-500 h-[55px] w-[100%]"
+  data-te-navbar-ref>
+  <div class="flex w-full flex-wrap items-center justify-between px-3">
+    <div>
+      <a
+        class="flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 lg:mb-0 lg:mt-0"
+        href="#">
+        <img
+          class="mr-1"
+          src="https://tecdn.b-cdn.net/img/logo/te-transparent-noshadows.webp"
+          style="height: 20px"
+          alt="TE Logo"
+          loading="lazy"/>
+      </a>
+    </div>
+
+    <!-- Hamburger button for mobile view -->
+    <button
+      class="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
+      type="button"
+      data-te-collapse-init
+      data-te-target="#navbarSupportedContent4"
+      aria-controls="navbarSupportedContent4"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <!-- Hamburger icon -->
+      <span class="[&>svg]:w-7">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="h-7 w-7">
+          <path
+            fill-rule="evenodd"
+            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+            clip-rule="evenodd" />
+        </svg>
+      </span>
+    </button>
+
+    <!-- Collapsible navbar container -->
+    <div
+      class="!visible hidden flex-grow basis-[100%] items-center lg:mt-0 lg:!flex lg:basis-auto"
+      id="navbarSupportedContent4"
+      data-te-collapse-item>
+      <!-- Left links -->
+      <ul
+        class="list-style-none mr-auto flex flex-col pl-0 lg:mt-1 lg:flex-row"
+        data-te-navbar-nav-ref>
+        <!-- Home link -->
+          <h2 class="companyName">lectrical and Mechanical Services</h2>
+      </ul>
+
+      <div class="flex items-center">
+      <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          class="buttons">
+          Home
+        </button>
+        <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          class="buttons">
+          About
+        </button>
+        <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          class="buttons">
+          Contacts
+        </button>
+      </div>
+    </div>
+  </div>
+</nav>
+<!--End of NavBar-->
+<!--Start of body-->
+<div class="body">
+
+  <div class="h-screen bg-white flex grid-cols-2">
+    <div class="box1 ">
+        <div class="row">
+          <h1 data-aos="fade-right" data-aos-delay="100" data-aos-duration="1000" class="text-5xl font-medium">We specialize in UI/UX, Web Development, Digital Marketing.</h1>
+        </div>
+        <div class="row">
+          <p data-aos="fade-right" data-aos-delay="300" data-aos-duration="1000" class="text-base	w-[80%]	h-[40px] text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla magna mauris. Nulla fermentum viverra sem eu rhoncus consequat varius nisi quis, posuere magna.</p>
+        </div>
+        <div data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000" class="row flex">
+            <div class="grid-cols-2 flex items-center">
+              <div>
+                  <button class="w-[200px] h-[50px] bg-red-500 rounded-[50px] bg-orange-500 text-white font-medium">Get Started Now</button>
+              </div>
+              <div class="pl-[50px]">
+                  <p class="font-medium">Call us (0123) 456 – 789</p>
+                  <p class="font-normal	text-gray-500	">For any question or concern</p>
+              </div>
+            </div>
+        </div>
+    </div>
+    <div class="box2">
+      <div class="circle">
+        <img class="marlou" src="{{ asset('images/marlou.png') }}">
+      </div>
+    </div>
+  </div>
+
+  <div class="h-screen	bg-white">
+  </div>
+
+  <div class="h-screen	bg-orange-500">
+  </div>
+
+  <div class="h-screen	bg-pink-500">
+  </div>
+
+
+  <script>
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    navbar.classList.add('bg-orange-500');
+    navbar.classList.remove('bg-transparent');
+  } else {
+    navbar.classList.add('bg-transparent');
+    navbar.classList.remove('bg-orange-500');
+  }
+});
+  </script>
+        <script src="bower_components/aos/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
 </div>
